bd4a858b03186565643188fc26115f311bf955b2
Oleksii Prosianko <oprosianko@roku.com>
2017-03-15 19:09:52 +0200

    Week 3. Quickcheck. QuickCheckHeap#genHeap implemented.

--- progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,7 +9,13 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      x <- arbitrary[Int]
+      h <- oneOf(const(empty), genHeap)
+    } yield insert(x, h)
+  )
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
