3c285401d4938ec2727f4f12b68e9494a055250a
oleksiypr <oleksiyprosyanko@gmail.com>
2017-04-02 23:16:57 +0300

    parprog1: VerticalBoxBlur#parBlur solved

--- parprog1/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
+++ parprog1/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -61,8 +61,13 @@ object VerticalBoxBlur {
    *  columns.
    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-    // TODO implement using the `task` construct and the `blur` method
-    ???
+    val dx = src.width / numTasks
+    val range = 0 to src.width by dx
+    val tasks = (range zip range.tail) map {
+      case (from, end) => task {
+        blur(src, dst, from, end, radius)
+      }
+    }
+    tasks.foreach(_.join())
   }
-
 }
