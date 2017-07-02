2a6f3e373d13094e690e355943844b05875ff7b4
oleksiypr <oleksiyprosyanko@gmail.com>
2017-07-02 13:32:06 +0300

    observatory/week1: Milestone 2 'Visualization#predictTemperature' solved

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -1,19 +1,53 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import scala.math._
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
 
+  val minDist: Double = (1000.0/1852.0/60.0).toRadians
+  val p = 3.0d
+
+  def dist(
+      location1: Location,
+      location2: Location
+    ): Double = {
+
+    val w1 = location1.lat.toRadians
+    val w2 = location2.lat.toRadians
+    val dl = (location1.lon - location2.lon).toRadians
+
+    acos(sin(w1)*sin(w2) + cos(w1)*cos(w2)*cos(dl))
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+  def predictTemperature(
+      temperatures: Iterable[(Location, Double)],
+      location: Location
+    ): Double = {
+
+    def weight(x: Location) = 1.0 / pow(dist(location, x), p)
+    def closeEnough(x: (Location, Double)) = dist(location, x._1) < minDist
+
+    def weighted = {
+      val ws = temperatures.map(t => weight(t._1))
+      val tw = (temperatures.map(_._2) zip ws)
+        .map { case (t, w) => t * w }
+
+      tw.sum / ws.sum
+    }
+
+    temperatures find closeEnough match {
+      case Some(lt) => lt._2
+      case None => weighted
+    }
   }
 
   /**
--- observatory/src/test/scala/observatory/VisualizationTest.scala
+++ observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -8,6 +8,24 @@ import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
+  import Visualization._
+  import math._
 
+  test("predictTemperature at known point") {
+    val ts = List((Location(0.0, 0.0), 10.0))
+    val location = Location(0.0, 0.0)
 
+    assert(predictTemperature(ts, location) == 10.0)
+  }
+
+  test("predictTemperature closer to the closest point") {
+    val a = (Location(+15.000,  000.000), +15.0)
+    val b = (Location( 00.000, +020.000), +20.0)
+
+    val t = predictTemperature(List(a , b), Location(0.0, 0.0))
+    val dtA = t - a._2
+    val dtB = t - b._2
+
+    assert(abs(dtA) < abs(dtB))
+  }
 }
