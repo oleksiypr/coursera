51376c9f66bb6487b21298717f94311e5378fe79
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2017-09-04 20:45:38 +0300

    machine-learning: week4: oneVsAll solved

--- machine-learning/week4/ex3/oneVsAll.m
+++ machine-learning/week4/ex3/oneVsAll.m
@@ -50,17 +50,18 @@ X = [ones(m, 1) X];
 %
 
 
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 
-
-
-
-
-
-
-
+for k = 1:num_labels
+    theta = zeros(n + 1, 1);
+    [theta] = ...
+        fmincg (@(t)(lrCostFunction(t, X, (y == k), lambda)), ...
+             initial_theta, options);
+    
+    all_theta(k, :) = theta';
+end
 
 % =========================================================================
-
-
 end
