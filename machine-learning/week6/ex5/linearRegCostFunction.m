04d0eec700769b00cfb6495bb60228cc47a220d1
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2017-09-17 23:41:41 +0300

    machine-learning:week6:linearRegCostFunction: grad solved

--- machine-learning/week6/ex5/linearRegCostFunction.m
+++ machine-learning/week6/ex5/linearRegCostFunction.m
@@ -7,10 +7,14 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta);
 
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
+
+L = eye(n);
+L(1, 1) = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
@@ -23,11 +27,8 @@ e = X*theta - y;
 J = 1/(2*m) * sum(sum(e.^2)) + ...
     lambda / (2*m) * sum(sum(theta(2:end, :).^2));
 
-
-
-
+grad = 1/m * (X'*e + lambda*L*theta);
 % =========================================================================
 
 grad = grad(:);
-
 end
