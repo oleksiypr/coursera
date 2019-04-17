f8495a6096ad645e24ad0e8c8ffb773915a965e3
oleksiypr <oleksiyprosyanko@gmail.com>
2019-04-17 20:33:32 +0300

    algorithms-part2: week4: initial commit

---
+++ algs4partII/week4/src/BoggleSolver.java
@@ -0,0 +1,39 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ ******************************************************************************/
+
+public class BoggleSolver     {
+
+    /**
+     * Initializes the data structure using the given array of strings as the
+     * dictionary.
+     * We can assume each word in the dictionary contains only the uppercase
+     * letters A through Z.
+     * @param dictionary array of strings as the dictionary
+     */
+    public BoggleSolver(String[] dictionary) {
+
+    }
+
+    /**
+     *
+     * @param board a bord
+     * @return the set of all valid words in the given Boggle board, as an
+     */
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return null;
+    }
+
+    /**
+     * You can assume the word contains only the uppercase letters A through Z.
+     * @param word a word
+     * @return the score of the given word if it is in the dictionary,
+     * zero otherwise.
+     */
+    public int scoreOf(String word) {
+        return -1;
+    }
+}
+
