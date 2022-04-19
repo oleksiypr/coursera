ee1f1b3a723b6fb9226def9debfbcb3536739cc2
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2022-04-19 13:51:23 +0200

    scala-akka-reactive | week-1 | example: solved

--- scala-akka-reactive/week-1/example/src/main/scala/example/Lists.scala
+++ scala-akka-reactive/week-1/example/src/main/scala/example/Lists.scala
@@ -22,7 +22,8 @@ object Lists:
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int =
+    if (xs.isEmpty) 0 else xs.head + sum(xs.tail)
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,4 +38,13 @@ object Lists:
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int =
+    if (xs.isEmpty) throw new NoSuchElementException
+    else
+      val head = xs.head
+      val tail = xs.tail
+      if (tail.isEmpty) head else max(head, max(tail))
+
+  def max(x: Int, y: Int): Int = if (x > y) x else y
+
+
--- scala-akka-reactive/week-1/example/src/test/scala/example/ListsSuite.scala
+++ scala-akka-reactive/week-1/example/src/test/scala/example/ListsSuite.scala
@@ -21,7 +21,7 @@ class ListsSuite extends munit.FunSuite:
   }
 
   test("one plus one is three (0pts)?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 2 == 3) // This assertion fails! Go ahead and fix it.
   }
 
   /**
@@ -57,7 +57,7 @@ class ListsSuite extends munit.FunSuite:
    * when writing tests.
    */
   test("details why one plus one is not three (0pts)") {
-    assertEquals(1 + 1, 3) // Fix me, please!
+    assertEquals(1 + 2, 3) // Fix me, please!
   }
 
   /**
@@ -99,7 +99,7 @@ class ListsSuite extends munit.FunSuite:
    * every tested aspect of a method.
    */
   test("sum of a few numbers (10pts)") {
-    assert(sum(List(1,2,0)) == 3)
+    assert(sum(List(1, 2, 0)) == 3)
   }
 
   test("max of a few numbers (10pts)") {
