9f99eb1cc59e24afb8e727b61da343552fe9dfb6
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2022-05-02 20:18:44 +0200

    week-2: solved

--- scala-akka-reactive/week-2/actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ scala-akka-reactive/week-2/actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -51,7 +51,8 @@ class BinaryTreeSet extends Actor:
   import BinaryTreeSet.*
   import BinaryTreeNode.*
 
-  def createRoot: ActorRef = context.actorOf(BinaryTreeNode.props(0, initiallyRemoved = true))
+  def createRoot: ActorRef =
+    context.actorOf(BinaryTreeNode.props(0, initiallyRemoved = true))
 
   var root = createRoot
 
@@ -63,14 +64,33 @@ class BinaryTreeSet extends Actor:
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case operation: Operation => root ! operation
+    case GC                   =>
+      val newRoot = createRoot
+      root ! CopyTo(newRoot)
+      context.become(garbageCollecting(newRoot))
+  }
 
   // optional
   /** Handles messages while garbage collection is performed.
     * `newRoot` is the root of the new binary tree where we want to copy
     * all non-removed elements into.
     */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
+  def garbageCollecting(newRoot: ActorRef): Receive = {
+    case CopyFinished  =>
+      root ! PoisonPill
+      root = newRoot
+      while (pendingQueue.nonEmpty) {
+        val (operation, pq) = pendingQueue.dequeue
+        pendingQueue = pq
+        root ! operation
+      }
+      context become normal
+
+    case operation: Operation =>
+      pendingQueue = pendingQueue.enqueue(operation)
+  }
 
 
 object BinaryTreeNode:
@@ -101,12 +121,76 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor:
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case cont @ Contains(requester, id, _) =>
+      search(cont)(
+        finalAction = _  => requester ! ContainsResult(id, false),
+        foundAction = () => requester ! ContainsResult(id, !removed)
+      )
+
+    case ins @ Insert(requester: ActorRef, id: Int, elem: Int) =>
+      search(ins)(
+        finalAction = (p: Position) => {
+          subtrees += p -> context.actorOf(BinaryTreeNode.props(elem, initiallyRemoved = false))
+          requester ! OperationFinished(id)
+        },
+        foundAction = () => {
+          removed = false
+          requester ! OperationFinished(id)
+        })
+
+    case rem @ Remove(requester, id, _) =>
+      search(rem)(
+        finalAction = _  => requester ! OperationFinished(id),
+        foundAction = () => {
+          removed = true
+          requester ! OperationFinished(id)
+        })
+
+    case CopyTo(node) =>
+      if (!removed) node ! Insert(self, elem, elem)
+
+      val children = subtrees.values.toSet
+      if (removed && children.isEmpty) copyFinished()
+      else {
+        children foreach { _ ! CopyTo(node) }
+        context become copying(children, insertConfirmed = removed)
+      }
+  }
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
+  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = {
+    case CopyFinished =>
+      val rest = expected - sender()
+      if (rest.isEmpty && insertConfirmed) context.parent ! CopyFinished
+      else context become copying(rest, insertConfirmed)
 
+    case OperationFinished(_) =>
+      if (expected.isEmpty) context.parent ! CopyFinished
+      else context become copying(expected, insertConfirmed = true)
+  }
 
+  private def search(
+    operation: Operation
+  )(
+    finalAction: Position => Unit,
+    foundAction: () => Unit
+  ): Unit = {
+    val elem = operation.elem
+    if (elem > this.elem) check(Right)
+    if (elem < this.elem) check(Left)
+    if (elem == this.elem) foundAction()
+
+    def check(p: Position): Unit =
+      subtrees.get(p) match {
+        case Some(subTree) => subTree ! operation
+        case None => finalAction(p)
+      }
+  }
+
+  private def copyFinished(): Unit = 
+    context.parent ! CopyFinished
+    context.stop(self)
